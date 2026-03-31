e1bf060e12bae0c25f06f6b69b60273fd0a44ef5
adrianrs928222 <adrianrs928222@gmail.com>
2026-03-31 12:08:09 +0200

    Actualizar main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// CLICK CARTA
 const img = document.getElementById("cartaImg");
 
 if (img) {
@@ -5,3 +6,38 @@ if (img) {
     window.open(img.src, "_blank");
   });
 }
+
+// SCROLL SUAVE
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// HEADER DINÁMICO
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+// ANIMACIONES SCROLL
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".animate").forEach(el => {
+  observer.observe(el);
+});
